3838c5eed865bf7a24631595976ff17e201d6854
“pablo-desperados” <pblmjc@gmail.com>
2024-03-03 13:59:18 +0100

    first commit

---
+++ src/main/java/com/shopping/oop/wamazon/WamazonApplication.java
@@ -0,0 +1,13 @@
+package com.shopping.oop.wamazon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WamazonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WamazonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shopping/oop/wamazon/WamazonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopping.oop.wamazon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WamazonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
